cac726a09b3b79919b9890cf47e31ef1052eb96e
Kevin Barnes <kbarnes3@gmail.com>
2021-12-03 11:48:57 -0800

    Day 1 sample data works

--- day01_sonar_sweep_part1/src/main.rs
+++ day01_sonar_sweep_part1/src/main.rs
@@ -1,6 +1,17 @@
 #[allow(unused_imports)]
-use day01_sonar_sweep_common::{TEST};
+use day01_sonar_sweep_common::{SAMPLE_DATA};
 
 fn main() {
-    println!("Hello, world!");
+    let result: u32 = do_work(SAMPLE_DATA);
+    println!("{}", result);}
+
+fn do_work(data: [u32; 10]) -> u32 {
+    let mut increases = 0;
+    for i in 1..10 {
+        if data[i] > data[i-1] {
+            increases += 1;
+        }
+    }
+
+    increases
 }
