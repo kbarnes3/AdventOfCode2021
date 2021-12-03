5436c13d6a68a99044ebec9024179cb3914c4436
Kevin Barnes <kbarnes3@gmail.com>
2021-12-03 14:15:34 -0800

    Day 2 sample data works

--- day02_dive_common/src/lib.rs
+++ day02_dive_common/src/lib.rs
@@ -1,1 +1,24 @@
-pub const SAMPLE_DATA: &str = "Hello, world!";
+pub enum Direction {
+    Forward,
+    Down,
+    Up
+}
+
+pub struct Command {
+    pub direction: Direction,
+    pub distance: u32,
+}
+
+use Direction::*;
+
+// Substitute with
+// '<,'>s/^\(\w*\) \(\d*\)/    Command { direction: \1, distance: \2 },/ | '<,'>s/forward/Forward/ | '<,'>s/down/Down/ | '<,'>s/up/Up/
+
+pub const SAMPLE_DATA: [Command; 6] = [
+    Command { direction: Forward, distance: 5 },
+    Command { direction: Down, distance: 5 },
+    Command { direction: Forward, distance: 8 },
+    Command { direction: Up, distance: 3 },
+    Command { direction: Down, distance: 8 },
+    Command { direction: Forward, distance: 2 },
+];
--- day02_dive_part1/src/main.rs
+++ day02_dive_part1/src/main.rs
@@ -1,6 +1,22 @@
 #[allow(unused_imports)]
-use day02_dive_common::{SAMPLE_DATA};
+use day02_dive_common::{Command, Direction, SAMPLE_DATA};
 
 fn main() {
-    println!("{}", SAMPLE_DATA);
+    let result: u32 = do_work(SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work<const N: usize>(data: [Command; N]) -> u32 {
+    let mut position = 0;
+    let mut depth = 0;
+
+    for command in data {
+        match command.direction {
+            Direction::Forward => position += command.distance,
+            Direction::Down => depth += command.distance,
+            Direction::Up => depth -= command.distance
+        }
+    }
+
+    position * depth
 }
