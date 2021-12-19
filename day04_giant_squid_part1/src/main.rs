13723d8e0e17051f1837ca1cf0f04b457201fff1
Kevin Barnes <kbarnes3@gmail.com>
2021-12-18 20:53:39 -0500

    Day 04 sample works

--- day04_giant_squid_part1/src/main.rs
+++ day04_giant_squid_part1/src/main.rs
@@ -12,19 +12,37 @@ impl CalledBoard {
 }
 
 fn main() {
-    let result = do_work(SAMPLE_DATA);
+    let result = do_work(&SAMPLE_DATA);
     println!("{}", result);
 }
 
-fn do_work<const N: usize, const B: usize>(data: InputData<N, B>) -> u32 {
-    2
+fn do_work<const N: usize, const B: usize>(data: &InputData<N, B>) -> u32 {
+    let mut first_winner: (usize, u32) = (N + 1, 0);
+    for board in &data.boards {
+        let new_score = get_score_for_board(&data.draw_numbers, &board);
+        let (current_first_win_number, _) = first_winner;
+        let (new_board_win_number, _) = new_score;
+        if new_board_win_number < current_first_win_number {
+            first_winner = new_score;
+        }
+    }
+
+    let (_, score) = first_winner;
+    score
 }
 
-fn get_score_for_board<const N: usize>(draw_numbers: [u32; N], board: BingoBoard) -> (usize, u32) {
+fn get_score_for_board<const N: usize>(draw_numbers: &[u32; N], board: &BingoBoard) -> (usize, u32) {
     let mut called_board = CalledBoard::new();
 
-    for number in draw_numbers {
+    for i in 0..draw_numbers.len() {
+        let number = draw_numbers[i];
         mark_board(number, &board, &mut called_board);
+        if is_winning_board(&called_board) {
+            let score = get_board_score(board, &called_board);
+            let score = score * number;
+            return (i, score);
+        }
+
     }
     (0, 0)
 }
@@ -39,13 +57,42 @@ fn mark_board(drawn_number: u32, board: &BingoBoard, called_board: &mut CalledBo
     }
 }
 
-fn is_winning_board(called_board: &CallBoard) -> bool {
+fn is_winning_board(called_board: &CalledBoard) -> bool {
     for i in 0..BOARDSIZE {
         let mut winning = true;
         for j in 0..BOARDSIZE {
-            winning = winning & called_board
+            winning = winning & called_board.board[i][j];
+        }
+
+        if winning {
+            return winning;
+        }
+    }
+
+    for j in 0..BOARDSIZE {
+        let mut winning = true;
+        for i in 0..BOARDSIZE {
+            winning = winning & called_board.board[i][j];
+        }
+
+        if winning {
+            return winning;
         }
     }
 
     false
+}
+
+fn get_board_score(board: &BingoBoard, called_board: &CalledBoard) -> u32 {
+    let mut score: u32 = 0;
+
+    for i in 0..BOARDSIZE {
+        for j in 0..BOARDSIZE {
+            if !called_board.board[i][j] {
+                score += board.board[i][j]
+            }
+        }
+    }
+
+    score
 }
