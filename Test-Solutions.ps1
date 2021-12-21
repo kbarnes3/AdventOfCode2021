bac1bc7296065a4bd052af8a97d15d08e58ff33c
Kevin Barnes <kbarnes3@gmail.com>
2021-12-21 15:05:26 -0500

    Solving Day 7 Part 2

--- Test-Solutions.ps1
+++ Test-Solutions.ps1
@@ -13,7 +13,8 @@ $Programs =
     @{"name" = "day05_hydrothermal_venture_part2"; "output" = "17741"},
     @{"name" = "day06_lanternfish_part1"; "output" = "383160"},
     @{"name" = "day06_lanternfish_part2"; "output" = "1721148811504"},
-    @{"name" = "day07_the_treachery_of_whales_part1"; "output" = "344535"}
+    @{"name" = "day07_the_treachery_of_whales_part1"; "output" = "344535"},
+    @{"name" = "day07_the_treachery_of_whales_part2"; "output" = "95581659"}
 
 for ($i = 0; $i -lt $Programs.Length; $i++) {
     $program = $Programs[$i]
--- day07_the_treachery_of_whales_part2/src/main.rs
+++ day07_the_treachery_of_whales_part2/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use day07_the_treachery_of_whales_common::{SAMPLE_DATA, REAL_DATA};
 
@@ -7,12 +9,13 @@ fn main() {
 }
 
 fn do_work<const T: usize>(data: &[u32; T]) -> u32 {
+    let mut cached_moves: HashMap<u32, u32> = HashMap::new();
     let min = *(data.iter().min().unwrap());
     let max = *(data.iter().max().unwrap());
 
-    let mut best_cost = get_fuel_cost(data, min);
+    let mut best_cost = get_fuel_cost(data, min, &mut cached_moves);
     for i in min+1..=max {
-        let cost = get_fuel_cost(data, i);
+        let cost = get_fuel_cost(data, i, &mut cached_moves);
         if cost < best_cost {
             best_cost = cost;
         }
@@ -21,15 +24,40 @@ fn do_work<const T: usize>(data: &[u32; T]) -> u32 {
     best_cost
 }
 
-fn get_fuel_cost<const T: usize>(data: &[u32; T], target_pos: u32) -> u32 {
+fn get_fuel_cost<const T: usize>(data: &[u32; T], target_pos: u32, cached_moves: &mut HashMap<u32, u32>) -> u32 {
     let mut cost: u32 = 0;
 
     for current_pos in data {
+        let distance: u32;
+
         if *current_pos < target_pos {
-            cost += target_pos - *current_pos;
+            distance = target_pos - *current_pos;
         } else {
-            cost += *current_pos - target_pos;
+            distance = *current_pos - target_pos;
         }
+
+        cost += get_move_cost(distance, cached_moves)
+    }
+
+    cost
+}
+
+fn get_move_cost(distance: u32, cached_moves: &mut HashMap<u32, u32>) -> u32 {
+    match cached_moves.get(&distance) {
+        Some(cost) => return *cost,
+        None => {
+            let cost = compute_move_cost(distance);
+            cached_moves.insert(distance, cost);
+            return cost;
+        }
+    }
+}
+
+fn compute_move_cost(distance: u32) -> u32 {
+    let mut cost: u32 = 0;
+
+    for i in 1..=distance {
+        cost += i;
     }
 
     cost
