e78bee444fdebbf4631d2e41514f86d05438d5f5
Kevin Barnes <kbarnes3@gmail.com>
2021-12-22 15:47:46 -0500

    Starting Day 8 Part 1

---
+++ day08_seven_segment_search_part1/src/main.rs
@@ -0,0 +1,11 @@
+#[allow(unused_imports)]
+use day08_seven_segment_search_common::{SAMPLE_DATA};
+
+fn main() {
+    let result = do_work(&SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work(data: &u32) -> u32 {
+    *data
+}
