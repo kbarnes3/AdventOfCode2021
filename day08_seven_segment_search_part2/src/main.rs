e7e18c644f75a318cc3d4896a0a40b84f8696302
Kevin Barnes <kbarnes3@gmail.com>
2021-12-22 16:39:39 -0500

    Starting Day 8 Part 2

---
+++ day08_seven_segment_search_part2/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+
+#[allow(unused_imports)]
+use day08_seven_segment_search_common::{Display, SAMPLE_DATA, REAL_DATA};
+
+const EASY_SEGMENT_COUNTS: [usize; 4] = [2, 3, 4, 7];
+
+fn main() {
+    let result = do_work(&REAL_DATA);
+    println!("{}", result);
+}
+
+fn do_work<const T: usize>(data: &[Display; T]) -> usize {
+    let easy_segments: HashSet<usize> = get_easy_segment_hashset();
+    let mut easy_count: usize = 0;
+
+    for display in data {
+        for digit in display.output {
+            if easy_segments.contains(&digit.len()) {
+                easy_count += 1;
+            }
+        }
+    }
+
+    easy_count
+}
+
+fn get_easy_segment_hashset() -> HashSet<usize> {
+    let mut set: HashSet<usize> = HashSet::with_capacity(EASY_SEGMENT_COUNTS.len());
+
+    for count in EASY_SEGMENT_COUNTS {
+        set.insert(count);
+    }
+
+    set
+}
